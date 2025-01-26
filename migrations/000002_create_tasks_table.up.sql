a81e0b8e6290b46a0587e1bc91b7a8e4e5d2b1d2
Amitkumar631 <amitkhan1911@gmail.com>
2025-01-26 22:50:10 +0530

    updated the migration script for creating tasks table

--- migrations/000002_create_tasks_table.up.sql
+++ migrations/000002_create_tasks_table.up.sql
@@ -1,6 +1,6 @@
 CREATE TABLE tasks (
     id SERIAL PRIMARY KEY,                
-    user_id,                 
+    user_id int,                 
     title VARCHAR(255) NOT NULL,          
     description VARCHAR(255) NOT NULL,            
     is_completed BOOLEAN DEFAULT false,       
